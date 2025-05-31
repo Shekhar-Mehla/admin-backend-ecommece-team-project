bf785513e12b9a2f0be36819fe9c5ebceb754233
Mahesh792 <mawhesjung143@gmail.com>
2025-05-31 11:04:46 +1000

    reset password  and verify password complete

--- src/utility/bcrypt.js
+++ src/utility/bcrypt.js
@@ -4,3 +4,7 @@ export const hashPassword = (password) => {
   const hash = bcrypt.hashSync(password, 10);
   return hash;
 };
+// Compare password with hash:
+export const comparePassword = (password, hash) => {
+  return bcrypt.compareSync(password, hash);
+};
