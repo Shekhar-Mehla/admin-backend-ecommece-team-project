5aa237f2ffe4e610408e923bd55df0f5fd04fbbb
Shekhar Singh <smehla147@gmail.com>
2025-06-20 11:36:40 +1000

    crud opration completed for product

--- src/routes/imageRoute.js
+++ src/routes/imageRoute.js
@@ -1,17 +1,7 @@
 import express from "express";
 import { upload } from "../middlewares/mutler/multer.js";
-import { Error } from "mongoose";
+import imageController from "../controllers/imageController.js";
+
 export const imageRouter = express.Router();
 
-imageRouter.post("/", upload.array("profilePicture", 5), (req, res, next) => {
-  // steps to follow
-  // 1. get req.file
-  // 2. upload image to the cloudnary
-  // 3.after you get url then delete the file from your server and send image url to client
-  try {
-    throw new Error("error is happen");
-  } catch (error) {
-    error.statusCode = 400;
-    next(error);
-  }
-});
+imageRouter.post("/", upload.array("profilePicture"), imageController);
