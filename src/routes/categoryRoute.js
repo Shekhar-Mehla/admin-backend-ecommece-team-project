5d2627fe1cae6f7eb9a67569b16c4b9ba11fd865
Shekhar Singh <smehla147@gmail.com>
2025-06-25 01:20:27 +1000

    working on updating category

--- src/routes/categoryRoute.js
+++ src/routes/categoryRoute.js
@@ -2,10 +2,12 @@ import express from "express";
 import {
   createNewCategory,
   getALLCategoryController,
+  updateCategoryController,
 } from "../controllers/categoryController.js";
 const categoryRouter = express.Router();
 
 categoryRouter.post("/create-new-cateogry", createNewCategory);
 categoryRouter.get("/", getALLCategoryController);
+categoryRouter.patch("/updateCategory", updateCategoryController);
 
 export default categoryRouter;
