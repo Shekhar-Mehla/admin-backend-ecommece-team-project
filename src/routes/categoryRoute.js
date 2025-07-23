d43aef9338ad6c5bb91e006cd1027631d111a080
Shekhar Singh <smehla147@gmail.com>
2025-07-24 09:32:36 +1000

    changes to category routes

--- src/routes/categoryRoute.js
+++ src/routes/categoryRoute.js
@@ -1,14 +1,8 @@
 import express from "express";
 
-import {
-  createNewCategory,
-  getALLCategoryController,
-  updateCategoryController,
-} from "../controllers/categoryController.js";
+import { createNewCategory } from "../controllers/categoryController.js";
 export const categoryRouter = express.Router();
 
 categoryRouter.post("/addCategory", createNewCategory);
-categoryRouter.get("/", getALLCategoryController);
-categoryRouter.patch("/updateCategory", updateCategoryController);
 
 export default categoryRouter;
