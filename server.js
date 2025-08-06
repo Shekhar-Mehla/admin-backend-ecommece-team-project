c0ffaf66c82b0d0c583da22d32ed7fd9d15f5a76
Mahesh792 <mawhesjung143@gmail.com>
2025-08-06 22:43:01 +1000

    review page integrated

--- server.js
+++ server.js
@@ -5,7 +5,6 @@ import path from "path";
 import cors from "cors";
 import "dotenv/config";
 
-
 import errorMiddleware from "./src/middlewares/errorMiddleware.js";
 import { authRouter } from "./src/routes/authRoutes.js";
 import productRouter from "./src/routes/productRoutes.js";
@@ -16,7 +15,7 @@ import categoryRouter from "./src/routes/categoryRoute.js";
 import orderRouter from "./src/Routes/orderRoutes.js";
 
 import imageRouter from "./src/Routes/imageRoute.js";
-
+import reviewRouter from "./src/Routes/reviewRoutes.js";
 
 const app = express();
 const PORT = process.env.PORT || 8000;
@@ -25,7 +24,6 @@ const PORT = process.env.PORT || 8000;
 app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
-
 
 // cloudnary cinfiguration
 // cloudnaryConfig();
@@ -46,6 +44,7 @@ app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/product", productRouter);
 app.use("/api/v1/category", categoryRouter);
 app.use("/api/v1/order", orderRouter);
+app.use("/api/v1/review", reviewRouter);
 
 // connect MongoDB
 connection()
